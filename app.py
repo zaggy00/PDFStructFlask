d66a1c11be87ca1cfd2492e7868010a90144a705
zaggy00 <zaggy00@protonmail.com>
2023-10-02 22:57:32 -0500

    Added drop down for models, finished front end

--- app.py
+++ app.py
@@ -1,51 +1,77 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify, render_template, send_file
 from werkzeug.utils import secure_filename
 import os
-import pdf_struct
+import pandas as pd
 from flask_cors import CORS
+import pdf_struct  # <-- Newly imported
+from subprocess import run
+
+
+def run_pdf_struct(model_name, pdf_path):
+    run(["pdf-struct", "predict", "--model", model_name, pdf_path])
+
+
+def process_pdf(path_to_pdf_file, model_name):  # <-- Included here
+    result = pdf_struct.predict(
+        format='paragraphs',
+        in_path=path_to_pdf_file,
+        model=model_name
+    )
+    return result  # <-- Result will be whatever pdf_struct returns
 
 
 app = Flask(__name__)
-
-
-@app.route('/', methods=['GET'])
-def index():
-    return render_template('index.html')
-
-
 CORS(app)
 UPLOAD_FOLDER = './uploads'
 ALLOWED_EXTENSIONS = {'pdf'}
 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+# In-memory storage for the parsed PDF data
+parsed_pdf_data = None
 
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/', methods=['GET'])
+def home():
+    return render_template('index.html')
 
 
 @app.route('/upload_pdf', methods=['POST'])
 def upload_pdf():
-    if 'pdf_file' not in request.files:
-        return 'No file part'
+    global parsed_pdf_data  # Note: Be cautious when using global variables.
     file = request.files['pdf_file']
-    if file.filename == '':
-        return 'No selected file'
-    if file and allowed_file(file.filename):
+
+    if file and file.filename.endswith('.pdf'):
         filename = secure_filename(file.filename)
         pdf_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
         file.save(pdf_path)
-        # Your PDF parsing logic here
 
-        # Use pdf-struct to predict the structure
-        result = pdf_struct.predict(
-            format='paragraphs',
-            in_path=pdf_path,
-            model='PDFContractEnFeatureExtractor'
-        )
+        default_model = 'PDFContractEnFeatureExtractor'
+        selected_model = request.form.get('model_name', default_model)
 
+        # After saving, proceed with parsing
+        result = process_pdf(format='paragraphs', in_path=pdf_path,
+                             model=selected_model)
+
+        parsed_pdf_data = result
         return jsonify({"result": result})
+    else:
+        return jsonify({"error": "Invalid file format"}), 400
+
+
+@app.route('/download_csv', methods=['GET'])
+def download_csv():
+    global parsed_pdf_data
+
+    if parsed_pdf_data is None:
+        return "No data to download"
+
+    # Convert the parsed PDF data to a DataFrame
+    df = pd.DataFrame(parsed_pdf_data)
+    csv_path = os.path.join(app.config['UPLOAD_FOLDER'], 'parsed_data.csv')
+    df.to_csv(csv_path, index=False)
+
+    return send_file(csv_path, as_attachment=True)
 
 
 if __name__ == '__main__':
